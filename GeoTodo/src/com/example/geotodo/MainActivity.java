b51c49ecae05040e062bfbf1f1d1bbf2f4c5322f
Sandra Liljeqvist <sandra.liljeqvist@gmail.com>
2015-08-24 13:01:42 +0200

    add a main activity with tabs

---
+++ GeoTodo/src/com/example/geotodo/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.geotodo;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+public class MainActivity extends FragmentActivity {
+	private TabsPagerAdapter mAdapter;
+	private ViewPager mViewPager;
+	private ActionBar actionBar;
+	private String[] tabs = { "Tasks", "Places" };
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Create ViewPager, connect to adapter and listen for page changes
+		mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mAdapter);
+		mViewPager
+				.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+					@Override
+					public void onPageSelected(int position) {
+						// When swiping between pages, select the
+						// corresponding tab.
+						getActionBar().setSelectedNavigationItem(position);
+					}
+				});
+
+		// Adding Tabs
+		actionBar = getActionBar();
+		actionBar.setHomeButtonEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		// Create a tab listener that is called when the user changes tabs.
+		ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+			@Override
+			public void onTabSelected(Tab tab, FragmentTransaction ft) {
+				// When the tab is selected, switch to the
+				// corresponding page in the ViewPager.
+				mViewPager.setCurrentItem(tab.getPosition());
+			}
+
+			@Override
+			public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+				// hide the given tab
+			}
+
+			@Override
+			public void onTabReselected(Tab tab, FragmentTransaction ft) {
+				// probably ignore this event
+			}
+		};
+
+		// Add tabs from the tabs array
+		for (String tabName : tabs) {
+			actionBar.addTab(actionBar.newTab().setText(tabName)
+					.setTabListener(tabListener));
+		}
+
+	}
+
+}
---
+++ GeoTodo/src/com/example/geotodo/TestFragment2.java
@@ -0,0 +1,20 @@
+package com.example.geotodo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TestFragment2 extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_test2, container,
+				false);
+
+		return rootView;
+	}
+}
