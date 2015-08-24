8f887697c64b5db09b0c2c77c431ad125f7e1ecc
Sandra Liljeqvist <sandra.liljeqvist@gmail.com>
2015-08-24 19:25:54 +0200

    change strategy for getting location by getting the last known location

--- GeoTodo/src/com/example/geotodo/LocationFragment.java
+++ GeoTodo/src/com/example/geotodo/LocationFragment.java
@@ -1,82 +1,68 @@
 package com.example.geotodo;
 
-import android.content.Context;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-public class LocationFragment extends Fragment {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
 
+public class LocationFragment extends Fragment implements ConnectionCallbacks,
+		OnConnectionFailedListener {
+
+	private static final String TAG = "LastKnownLocation";
+
+	private GoogleApiClient mGoogleApiClient;
 	private Location mLastLocation;
-	private TextView mLatitudeText;
-	private TextView mLongitudeText;
-	private LocationManager mLocationManager;
 
-	private final LocationListener mLocationListener = new LocationListener() {
-
-		@Override
-		public void onLocationChanged(Location locFromGps) {
-			// called when the listener is notified with a location update from
-			// the GPS
-			Log.d("log", "onLocationChanged()");
-			double currentLat = locFromGps.getLatitude();
-			double currentLong = locFromGps.getLongitude();
-			Log.d("log", "latitude: " + currentLat + " longitude: "
-					+ currentLong);
-			TestFragment2 secondFragment = new TestFragment2();
-			getActivity().getSupportFragmentManager().beginTransaction()
-					.add(R.id.fragment_container, secondFragment).commit();
-		}
-
-		@Override
-		public void onProviderDisabled(String provider) {
-			// called when the GPS provider is turned off (user turning off the
-			// GPS on the phone)
-			Log.d("log", "onProviderDisabled()");
-		}
-
-		@Override
-		public void onProviderEnabled(String provider) {
-			// called when the GPS provider is turned on (user turning on the
-			// GPS on the phone)
-			Log.d("log", "onProviderEnabled()");
-		}
-
-		@Override
-		public void onStatusChanged(String provider, int status, Bundle extras) {
-			// called when the status of the GPS provider changes
-			Log.d("log", "onStatusChanged()");
-		}
-	};
+	protected synchronized void buildGoogleApiClient() {
+		mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+				.addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this)
+				.addApi(LocationServices.API).build();
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-
+		Log.d(TAG, "onCreateView()");
 		View v = inflater.inflate(R.layout.fragment_location_container,
 				container, false);
 
-		mLatitudeText = (TextView) v.findViewById(R.id.latitudeTextView);
-		mLongitudeText = (TextView) v.findViewById(R.id.longitudeTextView);
-
-		mLocationManager = (LocationManager) getActivity().getSystemService(
-				Context.LOCATION_SERVICE);
-
-		mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-				35000, 10, this.mLocationListener);
-
-		TestFragment1 firstFragment = new TestFragment1();
-		getActivity().getSupportFragmentManager().beginTransaction()
-				.add(R.id.fragment_container, firstFragment).commit();
+		buildGoogleApiClient();
+		mGoogleApiClient.connect();
 
 		return v;
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		mLastLocation = LocationServices.FusedLocationApi
+				.getLastLocation(mGoogleApiClient);
+		Log.d(TAG, "onConnected(), latitude: " + mLastLocation.getLatitude()
+				+ " longitude: " + mLastLocation.getLongitude());
+		if (mLastLocation != null) {
+			TestFragment2 firstFragment = new TestFragment2();
+			getActivity().getSupportFragmentManager().beginTransaction()
+					.add(R.id.fragment_container, firstFragment).commit();
+		}
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		Log.d(TAG, "onConnectionFailed()");
+	}
+
+	@Override
+	public void onConnectionSuspended(int arg0) {
+		Log.d(TAG, "onConnectionSuspended()");
 	}
 
 }
